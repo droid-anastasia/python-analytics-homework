635cdc59c31cb7f89a403d99a5ce28efe068fdba
droid-anastasia <anastasja.chervjak@gmail.com>
2026-05-18 19:33:51 +0300

    test update Kyiv sales

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
+data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1300, 950, 500]}
 df = pd.DataFrame(data)
 
 print("Продажі по містах:")
